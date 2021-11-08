b1f23e64e9d6124c5a95f8f64188065218756c7b
Stewart Wong <siwei.wong@gmail.com>
2021-11-08 12:28:52 +0800

    update noxfile and lock

--- noxfile.py
+++ noxfile.py
@@ -12,11 +12,11 @@ except ImportError:
     Nox failed to import the 'nox-poetry' package.
     Please install it using the following command:
     {sys.executable} -m pip install nox-poetry"""
-    raise SystemExit(dedent(message))
+    raise SystemExit(dedent(message)) from None
 
 
 package = "aiocloudflare"
-python_versions = ["3.9", "3.8", "3.7"]
+python_versions = ["3.9"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = ("pre-commit", "safety", "mypy", "tests")
 
